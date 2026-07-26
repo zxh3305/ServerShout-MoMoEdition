ddfa1e89101abbd3d605adb373c6ee61c0c1d66a
zxh_141 <zxh_141@outlook.com>
2026-07-26 15:25:21 +0800

    Updated build configuration for Velocity version

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -1,6 +1,4 @@
-plugins {
-    kotlin("kapt") version "1.9.21"
-}
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 dependencies {
     implementation(project(":common"))
@@ -9,7 +7,21 @@ dependencies {
     compileOnly("net.luckperms:api:5.4")
 }
 
-val targetJavaVersion = 17
-kotlin {
-    jvmToolchain(targetJavaVersion)
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
+    }
+}
+
+tasks.named<ShadowJar>("shadowJar") {
+    archiveClassifier.set("")
+    dependencies {
+        exclude(dependency("org.jetbrains.kotlin:kotlin-stdlib.*"))
+        exclude(dependency("org.slf4j:slf4j-api"))
+        exclude(dependency("org.yaml:snakeyaml"))
+        exclude(dependency("com.mysql:mysql-connector-j"))
+        exclude(dependency("com.zaxxer:HikariCP"))
+    }
+    relocate("io.github.theramu.dependencyloader", "io.github.theramu.servershout.dependencyloader")
+    relocate("org.bstats", "io.github.theramu.servershout.metrics")
 }
